da0f82a0a04e974ee73b4d12ff05b15e8884c8f8
jotaalvim <jotaalvim179@proton.me>
2026-01-28 10:31:00 +0000

    project structure, set up routes

---
+++ cmd/api/healthcheck.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) healthCheck(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprint(w, "status: available\n")
+	fmt.Fprintf(w, "enviroment %s\n", app.config.env)
+	fmt.Fprintf(w, "version %s\n", version)
+}
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,57 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
 )
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *slog.Logger
+}
 
 func main() {
 
-	fmt.Println("teste")
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: false,
+		Level:     slog.LevelDebug, // Debug (descartadas), Info, Warn, Error
+	}))
+
+	var config config
+
+	flag.IntVar(&config.port, "port", 4000, "HTTP network adress  ")
+	flag.StringVar(&config.env, "env", "development", "Enviroment(developement|staging|production")
+	flag.Parse()
+
+	app := &application{
+		config: config,
+		logger: logger,
+	}
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", app.config.port),
+		Handler:      app.routes(),
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+		IdleTimeout:  1 * time.Minute,
+		ReadTimeout:  2 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	logger.Info("Starting server", slog.String("hosted_at", "https://localhost"+srv.Addr))
+
+	err := srv.ListenAndServe()
+	logger.Error(err.Error())
+	os.Exit(1)
 
 }
