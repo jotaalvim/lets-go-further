19fd1d3b315d95a24441325edfe4796e3a0e1589
João Alvim <joaoafonsoalvim@gmail.com>
2026-02-02 11:19:08 +0000

    Jotaalvim/setting up postgres (#3)
    * connecting to the database
    
    * configuring postgress database
    
    ---------
    
    Co-authored-by: jotaalvim <jotaalvim179@proton.me>

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+
+	//Postgrees go driver
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,6 +19,13 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+
+	db struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  time.Duration
+	}
 }
 
 type application struct {
@@ -32,7 +44,21 @@ func main() {
 
 	flag.IntVar(&config.port, "port", 4000, "HTTP network adress  ")
 	flag.StringVar(&config.env, "env", "development", "Enviroment(developement|staging|production")
+
+	flag.StringVar(&config.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_NAME"), "PostgresSQL")
+
+	flag.IntVar(&config.db.maxOpenConns, "db-max-open-conns", 25, "Postgres max amount of open connection")
+	flag.IntVar(&config.db.maxIdleConns, "db-max-idle-conns", 25, "Postgres max amount of connection idle")
+	flag.DurationVar(&config.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "Postgres max connection idle time")
+
 	flag.Parse()
+
+	db, err := openDB(config)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
 
 	app := &application{
 		config: config,
@@ -48,10 +74,38 @@ func main() {
 		WriteTimeout: 10 * time.Second,
 	}
 
+	logger.Info("database conection established")
 	logger.Info("Starting server", slog.String("hosted_at", "http://localhost"+srv.Addr))
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Error(err.Error())
 	os.Exit(1)
+
+}
+
+func openDB(config config) (*sql.DB, error) {
+
+	db, err := sql.Open("postgres", config.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetConnMaxIdleTime(config.db.maxIdleTime)
+	db.SetMaxOpenConns(config.db.maxOpenConns)
+	db.SetMaxIdleConns(config.db.maxIdleConns)
+
+	// create a context
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+	// if the connection was not successful in 5 seconds this returns an error
+	err = db.PingContext(ctx)
+
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module greenlight
 
 go 1.25.6
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require (
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/lib/pq v1.11.1 // indirect
+)
