c549d02fc36a9ce84d37537c54777e1db20bba43
João Alvim <joaoafonsoalvim@gmail.com>
2026-02-02 14:05:11 +0000

    database creations and drops (#4)
    Co-authored-by: jotaalvim <jotaalvim179@proton.me>

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -94,7 +94,7 @@ func openDB(config config) (*sql.DB, error) {
 	db.SetMaxOpenConns(config.db.maxOpenConns)
 	db.SetMaxIdleConns(config.db.maxIdleConns)
 
-	// create a context
+	// create a contex
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 
 	defer cancel()
