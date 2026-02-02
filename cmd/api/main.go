8c3e40918ad8b2819e3fe39701b75e2fa7013503
jotaalvim <jotaalvim179@proton.me>
2026-02-02 10:48:06 +0000

    connecting to the database

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+
+	//Postgrees go driver
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,6 +19,10 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+
+	db struct {
+		dsn string
+	}
 }
 
 type application struct {
@@ -32,7 +41,18 @@ func main() {
 
 	flag.IntVar(&config.port, "port", 4000, "HTTP network adress  ")
 	flag.StringVar(&config.env, "env", "development", "Enviroment(developement|staging|production")
+
+	// flag.StringVar(&config.db.dsn, "db-dsn", "postgres://greenlight:pass@localhost/greenlight?sslmode=disable", "PostgresSQL")
+	flag.StringVar(&config.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_NAME"), "PostgresSQL")
+
 	flag.Parse()
+
+	db, err := openDB(config)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
 
 	app := &application{
 		config: config,
@@ -48,10 +68,35 @@ func main() {
 		WriteTimeout: 10 * time.Second,
 	}
 
+	logger.Info("database conection established")
 	logger.Info("Starting server", slog.String("hosted_at", "http://localhost"+srv.Addr))
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Error(err.Error())
 	os.Exit(1)
+
+}
+
+func openDB(config config) (*sql.DB, error) {
+
+	db, err := sql.Open("postgres", config.db.dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// create a context
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+	// if the connection was not successful in 5 seconds this returns an error
+	err = db.PingContext(ctx)
+
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 
 }
