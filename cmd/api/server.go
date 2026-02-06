74366a07b0c40b87a2f424576b54c6e478376081
João Alvim <joaoafonsoalvim@gmail.com>
2026-02-06 10:10:09 +0000

    Jotaalvim/beutiful shutdown (#9)
    * catching signals
    
    * executing the shutdown
    
    ---------
    
    Co-authored-by: jotaalvim <jotaalvim179@proton.me>

---
+++ cmd/api/server.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func (app *application) serve() error {
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", app.config.port),
+		Handler:      app.routes(),
+		ErrorLog:     slog.NewLogLogger(app.logger.Handler(), slog.LevelError),
+		IdleTimeout:  1 * time.Minute,
+		ReadTimeout:  2 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	shutdownError := make(chan error)
+
+	// start a background goroutine
+	go func() {
+		quit := make(chan os.Signal, 1)
+
+		// notify listens to incomimg singint and sigterm and relay them to the quit channel.
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		// blocks until it's possible to read a sigal from the channel
+		s := <-quit
+
+		app.logger.Info("shutingdown server", "signal", s.String())
+		// exits sucessfuly
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+		shutdownError <- srv.Shutdown(ctx)
+	}()
+
+	app.logger.Info("Starting server", slog.String("hosted_at", "http://localhost"+srv.Addr))
+
+	// calling Shutdown will immidatly return an err on Listen and Server()
+	err := srv.ListenAndServe() //blocks
+
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	// er wait to receive a shutdown error from its channel
+	err = <-shutdownError
+	if err != nil {
+		return err
+	}
+
+	app.logger.Info("Stopped server", "addr", srv.Addr)
+
+	return nil
+}
